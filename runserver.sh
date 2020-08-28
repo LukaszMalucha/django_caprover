5b978a1c1e2d9d9d474ae1e14840f336b301e075
Lukasz Malucha <lucasmalucha@gmail.com>
2020-08-28 14:12:52 +0100

    setup

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,32 @@
-#!/bin/sh
+{
+  "schemaVersion": 2,
+  "dockerfileLines": [
+      "FROM library/python:3.7.4",
+      "RUN mkdir -p /usr/src/app",
+      "WORKDIR /usr/src/app",
 
-python manage.py migrate
-python manage.py makesuper
-gunicorn django_caprover.wsgi --bind=0.0.0.0:80
+      "COPY ./requirements.txt /requirements.txt",
+      "RUN pip install -r /requirements.txt",
+
+      "COPY . /usr/src/app",
+
+      "EXPOSE 80",
+      "COPY ./runserver.sh /usr/local/bin/",
+      "CMD [\"sh\", \"runserver.sh\"]"
+
+      "COPY ./requirements.txt /requirements.txt",
+      "RUN pip install -r /requirements.txt",
+      "RUN useradd -ms /bin/bash admin",
+      "RUN mkdir /app",
+      "COPY ./app/ /app",
+      "WORKDIR /app",
+      "RUN chown -R admin:admin /app",
+      "RUN chmod 755 /app",
+      "USER admin",
+
+
+      "RUN chmod 755 runserver.sh",
+
+  ]
+
+}
