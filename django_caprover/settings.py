3abf3c84f27d1ce3b56b8d3608a3aa81fc235e0d
Lukasz Malucha <lucasmalucha@gmail.com>
2020-08-28 14:17:24 +0100

    data

--- django_caprover/settings.py
+++ django_caprover/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 env = environ.Env(DEBUG=(bool, False))
 # env_file = os.path.join(BASE_DIR, ".env")
 # environ.Env.read_env(env_file)
-environ.Env.read_env()
+# environ.Env.read_env()
+
 
 DEBUG = env("DEBUG")
 SECRET_KEY = env("SECRET_KEY")
