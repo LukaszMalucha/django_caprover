d2f638687e4c9176bd3d5d32bb6ae0e5dd11e302
Lukasz Malucha <lucasmalucha@gmail.com>
2020-08-28 14:03:49 +0100

    setup

--- django_caprover/settings.py
+++ django_caprover/settings.py
@@ -17,10 +17,9 @@ import environ
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 env = environ.Env(DEBUG=(bool, False))
-env_file = os.path.join(BASE_DIR, ".env")
-
-environ.Env.read_env(env_file)
-
+# env_file = os.path.join(BASE_DIR, ".env")
+# environ.Env.read_env(env_file)
+environ.Env.read_env()
 
 DEBUG = env("DEBUG")
 SECRET_KEY = env("SECRET_KEY")
--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 python manage.py migrate
-python mange.py makesuper
+python manage.py makesuper
 gunicorn django_caprover.wsgi --bind=0.0.0.0:80
